0f1a28e1b27bff12ba191f026c57fa577fdce8c5
bedrantirak <bdrntrk@gmail.com>
2020-10-22 01:11:32 +0300

    Update App.js

--- photo-ai-w-reactapi/src/App.js
+++ photo-ai-w-reactapi/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+
+import React,{useState} from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const[image,setImage] = useState("")
+
+  const handleChange = ()=>{
+   // https://api.generated.photos/api/v1/faces?api_key=YOUR_API_KEY  , you should get your api key from generated photo and paste the link.
+    axios.get("https://api.generated.photos/api/v1/faces?api_key=YOUR_API_KEY&order_by=random").then(res =>{
+      const uri = res.data.faces[0].urls[4][512]
+      uri && setImage(uri)
+    }).catch(err =>{
+
+    })
+  }
+
   return (
     <div className="App">
-    
+    <h1>AI Photo Generator</h1>
+    {image && <img src={image} alt="AI Face" />}
+    <button type="button" onClick={handleChange}>
+      New Image
+    </button>
     </div>
   );
 }
